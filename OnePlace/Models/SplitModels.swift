c60a7374940a72a6af14e8a1f092199ca2305cfe
Majdi Nagi <majdinagi@gmail.com>
2026-02-08 14:23:51 -0500

    Fix SwiftData Split models

--- OnePlace/Models/SplitModels.swift
+++ OnePlace/Models/SplitModels.swift
@@ -5,16 +5,9 @@ import SwiftData
 final class SplitPerson {
     var id: UUID = UUID()
     var name: String = ""
-    @Relationship(deleteRule: .nullify, inverse: \SplitExpense.participants) var expenses: [SplitExpense]?
-    @Relationship(deleteRule: .nullify, inverse: \SplitExpense.paidBy) var expensesPaid: [SplitExpense]?
 
-    init(
-        id: UUID = UUID(),
-        name: String = ""
-    ) {
-        self.id = id
-        self.name = name
-    }
+    var expenses: [SplitExpense]?
+    var expensesPaid: [SplitExpense]?
 }
 
 @Model
@@ -23,18 +16,7 @@ final class SplitExpense {
     var title: String = ""
     var amount: Double = 0
     var date: Date = Date()
-    @Relationship(deleteRule: .nullify, inverse: \SplitPerson.expenses) var participants: [SplitPerson]?
-    @Relationship(deleteRule: .nullify, inverse: \SplitPerson.expensesPaid) var paidBy: SplitPerson?
 
-    init(
-        id: UUID = UUID(),
-        title: String = "",
-        amount: Double = 0,
-        date: Date = Date()
-    ) {
-        self.id = id
-        self.title = title
-        self.amount = amount
-        self.date = date
-    }
+    var participants: [SplitPerson]?
+    var paidBy: SplitPerson?
 }
